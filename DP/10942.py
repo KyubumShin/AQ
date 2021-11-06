531b81888ae99039ed960f408a5befdf203fe465
Kyubum Shin <kbum0617@naver.com>
2021-11-06 16:43:24 +0900

    DP update

---
+++ DP/10942.py
@@ -0,0 +1,28 @@
+import sys
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+
+def dp():
+    nums_dp = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(0, n):
+        for j in range(0, n-i):
+            if i == 0:
+                nums_dp[j][j+i] = 1
+            elif nums[j] == nums[j+i]:
+                if j+1 == j+i:
+                    nums_dp[j][j+1] = 1
+                elif nums_dp[j+1][j+i-1] == 1:
+                    nums_dp[j][j+i] = 1
+    return nums_dp
+
+n = int(input())
+nums = list(map(int, input().split()))
+dp_list = dp()
+m = int(input())
+for _ in range(m):
+    s, e = map(int, input().split())
+    print(dp_list[s-1][e-1])
+
+
+
---
+++ DP/11049.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+n = int(input())
+row = []
+col = []
+for _ in range(n):
+    r, c = list(map(int, input().split()))
+    row.append(r)
+    col.append(c)
+
+dp = [[0 for _ in range(n)] for _ in range(n)]
+for i in range(1, n):
+    for start in range(0, n - i):
+        end = start + i
+        if start + 1 == end:
+            dp[start][end] = row[start] * col[start] * col[end]
+        else: # 4 abcd -> a(bcd) (ab)(cd) (abc)d
+            dp[start][end] = 1 << 31
+            for k in range(start, end):
+                dp[start][end] = min(dp[start][end], dp[start][k] + dp[k+1][end] + row[start] * col[k] * col[end])
+print(dp[0][-1])
