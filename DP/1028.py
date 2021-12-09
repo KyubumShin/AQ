64e115b80a68f0d086dcd9c3591872a9e5250af4
Kyubum Shin <kbum0617@naver.com>
2021-12-10 04:10:57 +0900

    update 12-10

--- DP/1028.py
+++ DP/1028.py
@@ -3,77 +3,34 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 r, c = map(int, input().split())
-# 메모리 초과 코드
-# dp_down = [[] for _ in range(min(r, c)//2 + 1)]
-# dp_up = [[] for _ in range(min(r, c)//2 + 1)]
-# mine_map = []
-#
-# for _ in range(r):
-#     mine_map.append(list(str(input())))
-#
-# for k in range(min(r, c)//2 + 1):
-#     if k == 0:
-#         for i in range(r):
-#             for j in range(c):
-#                 if mine_map[i][j] == '1':
-#                     dp_up[k].append((i, j))
-#                     dp_down[k].append((i, j))
-#     else:
-#         for row, col in dp_up[k-1]:
-#             if row < r-k and k-1 < col < c-k:
-#                 if mine_map[row+k][col-k] == '1' and mine_map[row+k][col+k] == '1':
-#                     dp_up[k].append((row, col))
-#         for row, col in dp_down[k-1]:
-#             if k < row and k-1 < col < c-k:
-#                 if mine_map[row-k][col-k] == '1' and mine_map[row-k][col+k] == '1':
-#                     dp_down[k].append((row, col))
-#
-# answer = 0
-# for k in range(min(r, c)//2, 0, -1):
-#     if answer > k:
-#         print(answer)
-#         break
-#     if len(dp_up[k]) == 0 or len(dp_down[k]) == 0:
-#         continue
-#     else:
-#         for row, col in dp_up[k]:
-#             if (row + 2*k, col) in dp_down[k]:
-#                 answer = k+1
-# else:
-#     print(answer)
-
+dp_right_down = [[0] * c for _ in range(r)]
+dp_left_down = [[0] * c for _ in range(r)]
 mine_map = []
-dp = [[[0, 0]] * c for _ in range(r)]
-
 for _ in range(r):
-    mine_map.append(list(str(input())))
+    mine_map.append(list(str(input().strip())))
 
-for k in range(min(r, c)//2 + 1):
-    if k == 0:
-        for i in range(r):
-            for j in range(c):
-                if mine_map[i][j] == '1':
-                    dp[i][j] = [1, 1]
-    else:
-        for i in range(r):
-            for j in range(c):
-                up, down = dp[i][j]
-                if up == k and i < r-k and k-1 < j < c-k:
-                    if mine_map[i+k][j-k] == '1' and mine_map[i+k][j+k] == '1':
-                        up += 1
-                if down == k and 2*k-1 < i and k-1 < j < c-k:
-                    if mine_map[i-k][j - k] == '1' and mine_map[i-k][j + k] == '1':
-                        down += 1
-                dp[i][j] = [up, down]
+for i in range(c):
+    dp_right_down[0][i] = int(mine_map[0][i])
+    dp_left_down[0][i] = int(mine_map[0][i])
 
-MAX = 0
-for i in range(r):
-    for j in range(c):
-        up, _ = dp[i][j]
-        if up > MAX:
-            for k in range(up-1, -1, -1):
-                if i+k*2 < r:
-                    _, down = dp[i+k*2][j]
-                    if down >= k:
-                        MAX = max(MAX, k+1)
-print(MAX)
+for row in range(1, r):
+    for col in range(c):
+        if mine_map[row][col] == '1':
+            dp_right_down[row][col] = dp_right_down[row-1][col-1] + 1 if col > 0 else 1
+            dp_left_down[row][col] = dp_left_down[row-1][col+1] + 1 if col < c-1 else 1
+
+answer = 0
+for row in range(r):
+    for col in range(c):
+        if dp_left_down[row][col] <= answer:
+            continue
+        else:
+            for i in range(dp_left_down[row][col], answer, -1):
+                try:
+                    if dp_right_down[row][col+(i-1)*2] >= i and dp_left_down[row+i-1][col+i-1] >= i and dp_right_down[row+i-1][col+i-1] >= i:
+                        answer = i
+                        break
+                except:
+                    continue
+
+print(answer)
