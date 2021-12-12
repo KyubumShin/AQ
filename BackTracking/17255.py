9d945b00f122c4fa248a4077cd8157ab5518e388
Kyubum Shin <kbum0617@naver.com>
2021-12-13 01:33:31 +0900

    update 12-12

--- BackTracking/17255.py
+++ BackTracking/17255.py
@@ -3,3 +3,16 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 N = str(input().strip())
+
+
+def check(N):
+    if len(N) == 1:
+        return 1
+    left = N[:-1]
+    right = N[1:]
+    if left != right:
+        return check(left) + check(right)
+    else:
+        return check(left)
+
+print(check(N))
