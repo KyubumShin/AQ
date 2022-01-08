bccf334ef1861677cdf7be477cfb9f8765bf2b05
Kyubum Shin <kbum0617@naver.com>
2022-01-08 14:51:12 +0900

    update 2022-01-08

---
+++ Data_structure/Heap/1766.py
@@ -0,0 +1,28 @@
+import sys
+import heapq
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+heap = []
+route = [[] for _ in range(n+1)]
+prev_q = [0 for _ in range(n+1)]
+answer = []
+for _ in range(m):
+    f, s = map(int, input().split())
+    route[f].append(s)
+    prev_q[s] += 1
+
+for i in range(1, n+1):
+    if prev_q == 0:
+        heapq.heappush(heap, i)
+
+while heap:
+    cur = heapq.heappop(heap)
+    answer.append(cur)
+    for i in route[cur]:
+        prev_q -= 1
+        if prev_q == 0:
+            heapq.heappush(i)
+
+print(' '.join(map(str, answer)))
