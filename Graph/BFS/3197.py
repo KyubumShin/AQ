4a9461ec4097a63463ee5581545ca375b55351bb
Kyubum Shin <kbum0617@naver.com>
2022-01-11 01:53:35 +0900

    update 2022-01-10

--- Graph/BFS/3197.py
+++ Graph/BFS/3197.py
@@ -3,36 +3,9 @@ from collections import deque
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
-r, c = map(int, input().split())
-obj = {'.': 1, 'L': 2, 'X': 0}
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-table = []
-swan = []
-swan_other = []
-swan_visit = [[0] * c for _ in range(r)]
-ice = []
-time = 0
-
-for i in range(r):
-    lst = list(map(lambda x: obj[x], list(input().strip())))
-    for j in range(len(lst)):
-        if lst[j] == 0:
-            ice.append([i, j])
-        elif lst[j] == 2:
-            if not swan:
-                swan.append([i, j])
-            else:
-                swan_other.append([i, j])
-            lst[j] = 1
-    table.append(lst)
-print(table)
-print(swan)
-print(swan_other)
-print(ice)
-
 
 def swan_bfs(swan_q: deque):
+    global swan_other
     ret_q = deque()
     while swan_q:
         cur = swan_q.popleft()
@@ -40,22 +13,72 @@ def swan_bfs(swan_q: deque):
             nx = cur[1] + dx[i]
             ny = cur[0] + dy[i]
             if 0 <= nx < c and 0 <= ny < r:
-                if swan_visit[ny][nx] == 1:
+                if ny == swan_other[0] and nx == swan_other[1]:
+                    return time
+                elif visit[ny][nx] == 1:
                     continue
-                elif table[ny][nx] == 1:
-                    swan_visit[ny][nx] = 1
+                elif table[ny][nx] == '.':
+                    visit[ny][nx] = 1
                     swan_q.append([ny, nx])
                 elif table[ny][nx] == 0:
                     ret_q.append(cur)
-                elif [ny, nx] == swan_other:
-                    return time
     return ret_q
 
 
-print(swan_bfs(deque(swan)))
+def melting_ice(ice_q: deque, first: bool):
+    ret_q = deque()
+    while ice_q:
+        cur = ice_q.popleft()
+        for i in range(4):
+            nx = cur[1] + dx[i]
+            ny = cur[0] + dy[i]
+            if 0 <= nx < c and 0 <= ny < r:
+                if first:
+                    if table[ny][nx] == 1:
+                        ret_q.append(cur)
+                        break
+                else:
+                    if [ny, nx] in ret_q:
+                        continue
+                    if table[ny][nx] == 'X':
+                        ret_q.append([ny, nx])
+    for y, x in ret_q:
+        table[y][x] = 1
+    return ret_q
 
-def ice_bfs(queue):
-    pass
+
+def main():
+    global time, swan_other
+    swan = deque()
+    ice = deque()
+    for i in range(r):
+        lst = list(input().strip())
+        for j in range(len(lst)):
+            if lst[j] == 'X':
+                ice.append([i, j])
+            elif lst[j] == 2:
+                if not swan:
+                    swan.append([i, j])
+                else:
+                    swan_other = [i, j]
+                lst[j] = 1
+        table.append(lst)
+    first = True
+    while isinstance(swan, deque) and time < 5:
+        swan = swan_bfs(swan)
+        ice = melting_ice(ice, first)
+        first = False
+        time += 1
+    else:
+        print(swan)
 
 
-
+if __name__ == "__main__":
+    r, c = map(int, input().split())
+    obj = {'.': 1, 'L': 2, 'X': 0}
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    table = []
+    visit = [[0] * c for _ in range(r)]
+    time = 0
+    main()
