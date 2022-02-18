0185557a586f5b3ceac92fca142601e7263169dd
Kyubum Shin <kbum0617@naver.com>
2022-02-18 16:46:27 +0900

    update 22-02-18

--- Graph/BFS/7576.py
+++ Graph/BFS/7576.py
@@ -3,8 +3,8 @@ from collections import deque
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
-dx = [1, -1, 0, 0, 0, 0]
-dy = [0, 0, 1, -1, 0, 0]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 cnt = 0
 
 
